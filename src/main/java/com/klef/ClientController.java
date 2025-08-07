5be67b4b6a1def19b9f65ef0ec46b3c1c9a1830b
Shruthika <2300032637@kluniversity.in>
2025-08-07 22:30:38 +0530

    Added backend

---
+++ src/main/java/com/klef/ClientController.java
@@ -0,0 +1,13 @@
+package com.klef;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+    @GetMapping("/")
+    public String home()
+    {
+    	return "hello";
+    }
+}
