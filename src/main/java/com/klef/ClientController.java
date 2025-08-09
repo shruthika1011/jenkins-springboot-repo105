5b6336e48902aaba6e62c140cddc7cb80bb016e2
Shruthika <shruthikapadma2004@gmail.com>
2025-08-09 13:04:46 +0530

    updated code

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -4,10 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ClientController {
-    @GetMapping("/")
-    public String home()
-    {
-    	return "hello";
-    }
+public class ClientController 
+{
+	@GetMapping("/")
+	public String home() {
+		return "CI/CD and Cloud DevOps";
+	}
+	@GetMapping("/demo")
+	public String demo() 
+	{
+		return "Jenkins Demo";
+	}
+	
+	@GetMapping("/test")
+	public String test() 
+	{
+		return "Jenkins test";
+	}
+
 }
